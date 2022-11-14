b558aceaddf73cb48f8739e2dd82416f10b14d93
Mahfuj-HSTU <rmahfujur16@gmail.com>
2022-11-15 02:51:35 +0600

    header

---
+++ src/Routes/Routes.js
@@ -0,0 +1,28 @@
+import { createBrowserRouter } from 'react-router-dom';
+import Main from '../layout/Main';
+import Blog from '../pages/Blog/Blog';
+import Home from '../pages/Home/Home';
+import Login from '../pages/Login/Login';
+
+const router = createBrowserRouter( [
+    {
+        path: '/',
+        element: <Main></Main>,
+        children: [
+            {
+                path: '/',
+                element: <Home></Home>
+            },
+            {
+                path: '/blog',
+                element: <Blog></Blog>
+            },
+            {
+                path: '/login',
+                element: <Login></Login>
+            }
+        ]
+    }
+] )
+
+export default router
